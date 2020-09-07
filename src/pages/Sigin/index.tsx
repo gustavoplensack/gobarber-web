0394b42a6354b694ac1512f29cc2a962ebe853aa
Gustavo <gustavoplensack@gmail.com>
2020-09-07 20:50:15 -0300

    Split button and input components

--- src/pages/Sigin/index.tsx
+++ src/pages/Sigin/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import { Container, Content, Background } from './styles';
 
 import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => (
   <Container>
@@ -12,11 +15,16 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu Login</h1>
 
-        <input placeholder="Email" />
+        <Input name="email" icon={FiMail} placeholder="Email" />
 
-        <input type="password" placeholder="Senha" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
--- src/pages/Sigin/styles.ts
+++ src/pages/Sigin/styles.ts
@@ -28,40 +28,6 @@ export const Content = styled.div`
       margin-bottom: 24px;
     }
 
-    input {
-      background: #232129;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      padding: 16px;
-      width: 100%;
-      color: #f4ede8;
-
-      &::placeholder {
-        color: #666360;
-      }
-
-      & + input {
-        margin-top: 16px;
-      }
-    }
-
-    button {
-      background: #ff9000;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      padding: 16px;
-      color: #312e38;
-      width: 100%;
-      font-weight: 500;
-      margin-top: 16px;
-
-      transition: background-color 200ms;
-
-      &:hover {
-        background: ${shade(0.2, '#ff9000')};
-      }
-    }
-
     a {
       color: #f4ede8;
       display: block;
@@ -77,24 +43,22 @@ export const Content = styled.div`
   }
 
   > a {
-      color: #ff9000;
-      display: block;
-      margin-top: 24px;
-      text-decoration: none;
-      transition: color 200ms;
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 200ms;
 
-      display: flex;
-      align-items: center;
+    display: flex;
+    align-items: center;
 
-      svg {
-        margin-right: 16px;
-      }
-
-      &:hover {
-        color: ${shade(0.2, '#ff9000')};
-      }
+    svg {
+      margin-right: 16px;
     }
 
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
   }
 `;
 
